b15d1d2cf57b447ddf6802ac7f46bfc0f1f7a992
fjxfirst <286913267@qq.com>
2020-06-25 17:19:38 +0800

    模拟登陆

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, {Request, NextFunction, Response} from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import router from './router';
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieSession({
+    name: 'session',
+    keys: ['name fjx'],
+    maxAge: 24 * 60 * 60 * 1000
+}));
 app.use((req: Request, res: Response, next: NextFunction) => {
     //当往req对象新添加属性时，由于声明文件中没有对应的属性，所以会报错,这时需要类型融合
     req.teacherName = 'tom';
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import {Router, Request, Response} from 'express';
-
+import fs from 'fs';
+import path from 'path';
 import Crowller from './crowller';
 import DouBanAnalyzer from "./DouBanAnalyzer";
 
@@ -13,26 +14,68 @@ interface RequestWithBody extends Request {
 
 const router = Router();
 router.get('/', (req: Request, res: Response) => {
-    res.send(`
-    <html>
-        <body>
-            <form method="post" action="/getData">
-                <input type="password" name="password"/>
-                <button>提交</button>
-            </form>
-        </body>
-    </html>  
-`);
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        res.send(`
+            <html>
+                <body>
+                    <a href="/getData">爬取数据</a>
+                    <a href="/showData">展示数据</a>
+                    <a href="/logout">退出登录</a>
+                </body>
+            </html>  
+        `);
+    } else {
+        res.send(`
+            <html>
+                <body>
+                    <form method="post" action="/login">
+                        <input type="password" name="password"/>
+                        <button>提交</button>
+                    </form>
+                </body>
+            </html>  
+        `);
+    }
 });
-
-router.post('/getData', (req: RequestWithBody, res: Response) => {
-    if (req.body.password === '123') {
+router.get('/logout', (req: RequestWithBody, res: Response) => {
+    const isLogin = req.session ? req.session.login : false;
+    if (req.session) {
+        req.session.login = undefined;
+    }
+    res.redirect('/');
+});
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        res.send('已经登录');
+    } else {
+        if (req.body.password === '123' && req.session) {
+            req.session.login = true;
+            res.send('登录成功');
+        } else {
+            res.send(`登录失败`);
+        }
+    }
+});
+router.get('/getData', (req: RequestWithBody, res: Response) => {
+    const isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
         const url = 'https://movie.douban.com/chart';  // 爬取豆瓣电影排行榜
         const analyzer = DouBanAnalyzer.getInstance();
         new Crowller(url, analyzer);
         res.send('getData success');
     } else {
-        res.send(`${req.teacherName}password error`);
+        res.send('请先登录再爬取');
+    }
+});
+router.get('/showData', (req: RequestWithBody, res: Response) => {
+    try {
+        const position = path.resolve(__dirname, '../data/movie.json');
+        const result = fs.readFileSync(position, 'utf-8');
+        res.json(JSON.parse(result));
+    } catch (e) {
+        res.send('尚未爬取到内容');
     }
 
 });
